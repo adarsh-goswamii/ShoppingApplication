a568a29b173effef25ca6dfa07e64a1619296a47
Adarsh Goswami <adarsh010120@gmail.com>
2020-12-17 17:30:20 +0530

    Navigation Drawer OnClick

--- app/src/main/java/com/example/shoppingapplication/MainActivity.java
+++ app/src/main/java/com/example/shoppingapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.shoppingapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
@@ -27,6 +31,37 @@ public class MainActivity extends AppCompatActivity {
                 R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.cart:
+                        Toast.makeText(MainActivity.this, "Cart Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.about_us:
+                        Toast.makeText(MainActivity.this, "About us Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.categories:
+                        Toast.makeText(MainActivity.this, "Categories Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.licences:
+                        Toast.makeText(MainActivity.this, "Licenses Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.terms:
+                        Toast.makeText(MainActivity.this, "Terms and Conditions Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+
+        FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new MainFragment());
+        transaction.commit();
     }
 
     private void initViews()
